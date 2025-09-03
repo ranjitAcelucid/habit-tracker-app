17058ed9e75cdf2d280dd895a9639e7a9a1bc2d9
Manoj-Simha-NN <manoj.simha@acelucid.com>
2025-09-03 13:25:40 +0530

    Initial commit

---
+++ app/(tabs)/add-habit.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function AddHabitScreen(){
+    return (
+        <View>
+            <Text> Hello this is the habit page</Text>
+        </View>
+    )
+}
---
+++ app/(tabs)/streaks.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function StreaksScreen(){
+    return (
+        <View>
+            <Text> Hello this is the streaks page</Text>
+        </View>
+    )
+}
